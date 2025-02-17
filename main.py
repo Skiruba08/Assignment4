3f0873ad41b3f6a5e4caedbf9d14a9edf005cd8a
Sherin_Kiruba <skirubap@charlotte.edu>
2025-02-16 19:32:38 -0500

    Sandwich Maker Assignment 2

---
+++ main.py
@@ -0,0 +1,36 @@
+import data, sandwich_maker, cashier
+
+
+# Make an instance of other classes here
+resources = data.resources
+recipes = data.recipes
+sandwich_maker_instance = sandwich_maker.SandwichMaker(resources)
+cashier_instance = cashier.Cashier()
+
+
+
+
+def main():
+        sandwich_maker_instance.is_on = True
+        while sandwich_maker_instance.is_on:
+            choice = input("What would you like? (small/medium/large/off/report): ").lower()
+
+            if choice == "off":
+                sandwich_maker_instance.is_on = False
+                print("Goodbye! Thanks for shopping!")
+            elif choice == "report":
+                for item, amount in resources.items():
+                    print(f"{item.capitalize()}: {amount}")
+            elif choice in recipes:
+                sandwich = recipes[choice]
+                if sandwich_maker_instance.check_resources(sandwich["ingredients"]):
+                    transaction = cashier_instance.process_coins()
+                    if cashier_instance.transaction_result(transaction, sandwich[
+                        "cost"]):
+                        sandwich_maker_instance.make_sandwich(choice, sandwich["ingredients"])
+            else:
+                print("Invalid choice. Please select again.")
+
+
+if __name__=="__main__":
+    main()
