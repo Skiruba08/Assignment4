62f1c08d1f277e60c66586ca1bf66614018d9846
Sherin_Kiruba <skirubap@charlotte.edu>
2025-03-10 17:12:58 -0400

    update function

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-import data, sandwich_maker, cashier
+import data
+import sandwich_maker
+import cashier
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Define a Pydantic model for item validation
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union [str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
 
 # Make an instance of other classes here
 resources = data.resources
 recipes = data.recipes
 sandwich_maker_instance = sandwich_maker.SandwichMaker(resources)
 cashier_instance = cashier.Cashier()
-
-
 
 
 def main():
